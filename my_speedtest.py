8123ee0d31de523c52cbfb21df62a718d32191a3
sudosammi <sudosammi@gmail.com>
2026-05-31 20:58:08 +0530

    edit script

--- my_speedtest.py
+++ my_speedtest.py
@@ -1,1 +1,22 @@
 import  speedtest
+
+print("Wait..., Speed Testing")
+
+st = speedtest.Speedtest()
+
+
+download_speed = st.download() / 10**6
+
+upload_speed = st.upload() / 10**6
+
+ping = st.results.ping 
+
+
+
+print("-" * 30)
+
+print(f"🔽 Download Speed : {download_speed:.2f} Mbps")
+print(f"🔼 Upload Speed   : {upload_speed:.2f} Mbps")
+print(f"📶 Ping / Latency : {ping} ms")
+
+print("-" * 30)
